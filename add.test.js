3948264b79f2df9f8857663e9953cd9b2b47dc58
shahzaib rehman <nk780877@gmail.com>
2026-08-06 00:25:53 +0500

    demo: add multiply() bug for a fresh GitHub self-heal test

--- add.test.js
+++ add.test.js
@@ -1,7 +1,11 @@
 const test = require('node:test');
 const assert = require('node:assert');
-const { add } = require('./add');
+const { add, multiply } = require('./add');
 
 test('add sums two numbers', () => {
   assert.strictEqual(add(2, 3), 5);
+});
+
+test('multiply multiplies two numbers', () => {
+  assert.strictEqual(multiply(4, 5), 20);
 });
